45a53200d7180c90b81f5233f776b8e35e554b3c
Nikhil Singh <nikhilsingh498@gmail.com>
2019-04-30 08:13:17 +0530

    Email Validation Added
    **rest all the inputs set to required

--- model/helpers.js
+++ model/helpers.js
@@ -4,3 +4,8 @@ module.exports.hashAndReturn = (password) => {
     let hash = bcrypt.hashSync(password, Number(process.env.SALT));
     return hash
 }
+
+module.exports.emailValidate = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,37 +1,41 @@
 const router = require("express").Router();
 const userProfile = require("../model/model.js").userProfile;
 const hashAndReturn = require("../model/helpers.js").hashAndReturn;
-
+const emailValidate = require("../model/helpers.js").emailValidate;
 
 
 
 router.post("/signup", (request, response) => {
 
-    let profile = new userProfile({
-        FIRSTNAME : request.body.firstname,
-        LASTNAME : request.body.lastname,
-        EMAIL : request.body.email,
-        USERNAME : request.body.username,
-        PASSWORD : hashAndReturn(request.body.password)
-    })
+    if (!emailValidate(request.body.email)){
+        console.log("There was error validating your email id")
+        response.send("There was error validating your email id")
+    } else {
 
-    profile.save((err, data) => {
-        if (err){
-            if (err.code === 11000){
-                console.log("The given email id is already registered with us")
-                response.send("The given email id is already registered with us")
+        let profile = new userProfile({
+            FIRSTNAME : request.body.firstname,
+            LASTNAME : request.body.lastname,
+            EMAIL : request.body.email,
+            USERNAME : request.body.username,
+            PASSWORD : hashAndReturn(request.body.password)
+        })
+    
+        profile.save((err, data) => {
+            if (err){
+                if (err.code === 11000){
+                    console.log("The given email id is already registered with us")
+                    response.send("The given email id is already registered with us")
+                } else {
+                    console.log("There was some error saving the profile details", err)
+                    response.send("There was some error signing you up :<")
+                }
             } else {
-                console.log("There was some error saving the profile details", err)
-                response.send("There was some error signing you up :<")
+                console.log("Profile saved in the database")
+                response.send("Data successfully saved")
             }
-        } else {
-            console.log("Profile saved in the database")
-            response.send("Data successfully saved")
-        }
-    })
-    
-    
+        })
 
+    }
 })
 
 
