e643bf71b3cbec33e6403860643580481adb9b4b
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-21 04:42:39 +0530

    Image upload feature added

---
+++ model/imageUpload.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: "quadro",
+    allowedFormats: ["jpg", "png", "jpeg"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }]
+});
+
+const parser = multer({ storage }); 
+
+module.exports.parser = parser;
+
+
+
--- routes/protected.js
+++ routes/protected.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const parser = require("../model/imageUpload.js").parser;
+const userProfile = require("../model/model.js").userProfile;
+
 
 router.get("/private", (request, response) => {
     response.json({
@@ -6,5 +9,32 @@ router.get("/private", (request, response) => {
         message : "This is the page displayed after authentication"
     })
 })
+
+router.post('/imageUpload', parser.single("image"), (request, response) => {
+    //let email = request.decode.email;
+    userProfile.findOneAndUpdate({ EMAIL : request.decode.email},{$set:{ IMAGE_URL : request.file.url, IMAGE_ID : request.file.public_id }},{new:true})   
+    .then((result)=>{
+        if(result) {
+            response.json({
+                status : 200,
+                message : "Profile pic was successfully updated"
+                //send url as well of the updated image so that profile pic can get updated 
+            })
+        } else {
+            response.json({
+                status : 400,
+                message : "There was authentication error while updating the profile pic"
+            })
+        }
+    })
+    .catch((error)=>{
+        response.json({
+            status : 500,
+            message : "There was some server error while updating profile pic"
+        })
+    })
+
+});
+
 
 module.exports = router;
