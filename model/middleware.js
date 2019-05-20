ae6c8afd64df8e197868bc72251aaae82173f8d7
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-21 03:57:20 +0530

    Middleware update

--- model/middleware.js
+++ model/middleware.js
@@ -2,21 +2,21 @@ const jwt = require("jsonwebtoken");
 
 module.exports.session = (request, response, next) => {
     let token = request.cookies.sessionJWT;
-    console.log(token);
-    console.log("Token banate hain")
     if (token) {
         jwt.verify(token , process.env.SECRET , function (error , decode){
             console.log(decode);
             if (error) {
-                response.send("The authentication failed, try logging")
+                response.send({
+                    status : 400,
+                    message : "Authentication failed (unable to authenticate access token)"
+                })
             }
             else{
                 request.decode = decode;
                 next();
             }
-        } )
-    }
-    else{
-        response.send("This is private page, you cannot access it without authenticating yourself")
+        })
+    } else{
+        next();
     }
 }
