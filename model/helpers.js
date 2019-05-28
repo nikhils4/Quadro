f9f4e1fdf022fa4635cc43574279774baa091ea9
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-28 06:23:43 +0530

    Add random password generator

--- model/helpers.js
+++ model/helpers.js
@@ -20,3 +20,11 @@ module.exports.passwordAuth = (dbPassword, inputPassword) => {
     }
 }
 
+module.exports.passwordGenerator = () => {
+    var text = "";
+    var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 8; i++)
+      text += charset.charAt(Math.floor(Math.random() * charset.length));
+    return text;
+}
+
